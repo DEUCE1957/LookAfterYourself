f78f3925d1430d61eb2b6396e6872889af5cdfbb
Team C Lab 3 <2246306W@student.gla.ac.uk>
2018-03-04 16:26:31 +0000

    URL configuration infrastructure complete

--- LookAfterYourself/urls.py
+++ LookAfterYourself/urls.py
@@ -13,11 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from MainApp import views
 
 urlpatterns = [
     url(r'^$',views.index, name='index'),
+    url(r'^LookAfterYourself/', include('MainApp.urls')),
     url(r'^admin/',admin.site.urls),
 ]
