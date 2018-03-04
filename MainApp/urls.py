75a2b192227c6e132632cede94e7a345eb9cbac5
Sohva <33163737+Sohva@users.noreply.github.com>
2018-03-04 16:58:27 +0000

    Added links to tips and blog

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -3,4 +3,6 @@ from MainApp import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^Blog', views.blog, name='blog'),
+    url(r'^Tips', views.tips, name='tips'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -4,4 +4,10 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("This is the Look After Yourself index page")
 
+def blog(request):
+    return HttpResponse("This is the Look After Yourself blog page")
+
+def tips(request):
+    return HttpResponse("This is the Look After Yourself tips page")
+
 # Create your views here.
