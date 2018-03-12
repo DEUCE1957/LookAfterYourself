73bfecb22a90abd63511c3b01259268282003087
Team C Lab 3 <2246306W@student.gla.ac.uk>
2018-03-12 15:31:26 +0000

    Working Carousel with some links, working menu bar links, home link when title is clicked

--- MainApp/views.py
+++ MainApp/views.py
@@ -50,7 +50,7 @@ def blog(request):
     return render(request,'MainApp/blog.html', context={})
 
 def tips(request):
-    return HttpResponse("This is the Look After Yourself tips page")
+    return HttpResponse(request,'MainApp/tips.html', context={})
 
 def support(request):
     return render(request,'MainApp/bootstrap.html', context={})
