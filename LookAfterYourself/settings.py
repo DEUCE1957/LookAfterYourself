59a9ce04dff118d13186a9c54bf551ad59ffcde1
Sohva <33163737+Sohva@users.noreply.github.com>
2018-03-19 23:52:13 +0000

    Added static directory to settings and moved calendar.css there

--- LookAfterYourself/settings.py
+++ LookAfterYourself/settings.py
@@ -16,6 +16,9 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+
 #Something along these lines for the Google key
 #GOOGLE_KEY = open('google.key', 'r').read().strip()
 
