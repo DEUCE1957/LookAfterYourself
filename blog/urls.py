84f7be5eba13e0516b28c619a419c4cfbee81b25
Illya <2257538p@student.gla.ac.uk>
2018-03-22 23:08:11 +0000

    removed unnecessary

--- blog/urls.py
+++
@@ -1,9 +0,0 @@
-# coding: utf-8
-from django.conf.urls import url
-
-from blog.views import PostsListView, PostDetailView
-
-urlpatterns = [
-    url(r'^$', PostsListView.as_view(), name='list'),
-    url(r'^(?P<pk>\d+)/$', PostDetailView.as_view()),
-]
